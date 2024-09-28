ded4dd5656e5cc8f980a3bfa85208b9ded9673f5
donny <donnyfe@qq.com>
2024-09-29 02:39:18 +0800

    fix: remoteHost variable

--- src/env.js
+++ src/env.js
@@ -1,67 +1,65 @@
 /**
  * @file Module used to configure Transformers.js.
- * 
+ *
  * **Example:** Disable remote models.
  * ```javascript
  * import { env } from '@xenova/transformers';
  * env.allowRemoteModels = false;
  * ```
- * 
+ *
  * **Example:** Set local model path.
  * ```javascript
  * import { env } from '@xenova/transformers';
  * env.localModelPath = '/path/to/local/models/';
  * ```
- * 
+ *
  * **Example:** Set cache directory.
  * ```javascript
  * import { env } from '@xenova/transformers';
  * env.cacheDir = '/path/to/cache/directory/';
  * ```
- * 
+ *
  * @module env
  */
 
-import fs from 'fs';
-import path from 'path';
-import url from 'url';
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
 
-import { ONNX } from './backends/onnx.js';
-const { env: onnx_env } = ONNX;
+import { ONNX } from './backends/onnx.js'
+const { env: onnx_env } = ONNX
 
-const VERSION = '2.17.2';
+const VERSION = '2.17.2'
 
 // Check if various APIs are available (depends on environment)
-const WEB_CACHE_AVAILABLE = typeof self !== 'undefined' && 'caches' in self;
-const FS_AVAILABLE = !isEmpty(fs); // check if file system is available
-const PATH_AVAILABLE = !isEmpty(path); // check if path is available
+const WEB_CACHE_AVAILABLE = typeof self !== 'undefined' && 'caches' in self
+const FS_AVAILABLE = !isEmpty(fs) // check if file system is available
+const PATH_AVAILABLE = !isEmpty(path) // check if path is available
 
-const RUNNING_LOCALLY = FS_AVAILABLE && PATH_AVAILABLE;
+const RUNNING_LOCALLY = FS_AVAILABLE && PATH_AVAILABLE
 
 // __dirname is reserved so we use dirname__ instead.
 const dirname__ = RUNNING_LOCALLY
-    ? path.dirname(path.dirname(url.fileURLToPath(import.meta.url)))
-    : './';
+	? path.dirname(path.dirname(url.fileURLToPath(import.meta.url)))
+	: './'
 
 // Only used for environments with access to file system
-const DEFAULT_CACHE_DIR = RUNNING_LOCALLY
-    ? path.join(dirname__, '/.cache/')
-    : null;
+const DEFAULT_CACHE_DIR = RUNNING_LOCALLY ? path.join(dirname__, '/.cache/') : null
 
 // Set local model path, based on available APIs
-const DEFAULT_LOCAL_MODEL_PATH = '/models/';
+const DEFAULT_LOCAL_MODEL_PATH = '/models/'
 const localModelPath = RUNNING_LOCALLY
-    ? path.join(dirname__, DEFAULT_LOCAL_MODEL_PATH)
-    : DEFAULT_LOCAL_MODEL_PATH;
+	? path.join(dirname__, DEFAULT_LOCAL_MODEL_PATH)
+	: DEFAULT_LOCAL_MODEL_PATH
 
 if (onnx_env?.wasm) {
-    // Set path to wasm files. This is needed when running in a web worker.
-    // https://onnxruntime.ai/docs/api/js/interfaces/Env.WebAssemblyFlags.html#wasmPaths
-    // We use remote wasm files by default to make it easier for newer users.
-    // In practice, users should probably self-host the necessary .wasm files.
-    onnx_env.wasm.wasmPaths = RUNNING_LOCALLY
-        ? path.join(dirname__, '/dist/')
-        : `https://cdn.jsdelivr.net/npm/@xenova/transformers@${VERSION}/dist/`;
+	// Set path to wasm files. This is needed when running in a web worker.
+	// https://onnxruntime.ai/docs/api/js/interfaces/Env.WebAssemblyFlags.html#wasmPaths
+	// We use remote wasm files by default to make it easier for newer users.
+	// In practice, users should probably self-host the necessary .wasm files.
+	onnx_env.wasm.wasmPaths = RUNNING_LOCALLY
+		? path.join(dirname__, '/dist/')
+		: `https://cdn.jsdelivr.net/npm/@xenova/transformers@${VERSION}/dist/`
 }
 
 /**
@@ -86,44 +84,43 @@ if (onnx_env?.wasm) {
  * implements the `match` and `put` functions of the Web Cache API. For more information, see https://developer.mozilla.org/en-US/docs/Web/API/Cache
  */
 export const env = {
-    /////////////////// Backends settings ///////////////////
-    backends: {
-        // onnxruntime-web/onnxruntime-node
-        onnx: onnx_env,
+	/////////////////// Backends settings ///////////////////
+	backends: {
+		// onnxruntime-web/onnxruntime-node
+		onnx: onnx_env,
 
-        // TensorFlow.js
-        tfjs: {},
-    },
+		// TensorFlow.js
+		tfjs: {},
+	},
 
-    __dirname: dirname__,
-    version: VERSION,
+	__dirname: dirname__,
+	version: VERSION,
 
-    /////////////////// Model settings ///////////////////
-    allowRemoteModels: true,
-    remoteHost: 'https://huggingface.co/',
-    remotePathTemplate: '{model}/resolve/{revision}/',
+	/////////////////// Model settings ///////////////////
+	allowRemoteModels: true,
+	// remoteHost: 'https://huggingface.co/', // Original
+	remoteHost: 'https://hf-mirror.com/', // use mirror website
+	remotePathTemplate: '{model}/resolve/{revision}/',
 
-    allowLocalModels: true,
-    localModelPath: localModelPath,
-    useFS: FS_AVAILABLE,
+	allowLocalModels: true,
+	localModelPath: localModelPath,
+	useFS: FS_AVAILABLE,
 
-    /////////////////// Cache settings ///////////////////
-    useBrowserCache: WEB_CACHE_AVAILABLE,
+	/////////////////// Cache settings ///////////////////
+	useBrowserCache: WEB_CACHE_AVAILABLE,
 
-    useFSCache: FS_AVAILABLE,
-    cacheDir: DEFAULT_CACHE_DIR,
+	useFSCache: FS_AVAILABLE,
+	cacheDir: DEFAULT_CACHE_DIR,
 
-    useCustomCache: false,
-    customCache: null,
-    //////////////////////////////////////////////////////
+	useCustomCache: false,
+	customCache: null,
+	//////////////////////////////////////////////////////
 }
-
 
 /**
  * @param {Object} obj
  * @private
  */
 function isEmpty(obj) {
-    return Object.keys(obj).length === 0;
+	return Object.keys(obj).length === 0
 }
-
